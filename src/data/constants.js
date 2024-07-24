913f0d1a19449d50bce48ce38311dfdada1cda02
mourya_peddineni <mouryap9999@gmail.com>
2024-07-24 23:55:54 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,234 @@
+export const Bio = {
+  name: "Mourya",
+  roles: ["Frontend enthusiast", "Coder", "Problem Solver", "Tech geek"],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/MouryaPeddineni",
+  resume:
+    "https://drive.google.com/file/d/1sh5ZOO90EEmdUpeHLaqpI7ObxSq533YQ/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/mourya-peddineni-b19115233/",
+  twitter: "https://twitter.com/MouryaP123",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Tailwind",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/3/3f/Git_icon.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+// export const experiences = [
+//   {
+//     id: 0,
+//     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
+//     role: "DevOps & Fullstack Engineering Intern",
+//     company: "Flipr Inovations Pvt. Ltd.",
+//     date: "Aug 2023 - July 2023",
+//     desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+//     skills: [
+//       "Docker",
+//       "Terraform",
+//       "AWS",
+//       "EC2",
+//       "Portainer",
+//       "Nginx",
+//       "JavaScript",
+//       "TypeScript",
+//       "Node Js",
+//       " Next Js",
+//     ],
+//     doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+//   },
+//   {
+//     id: 0,
+//     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+//     role: "Frontend Engineer Intern",
+//     company: "Neurobit",
+//     date: "June 2023 - Present",
+//     desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+//     skills: [
+//       "ReactJS",
+//       "Redux",
+//       "NodeJs",
+//       "Material UI",
+//       "HTML",
+//       "CSS",
+//       "JavaScript",
+//     ],
+//     doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+//   },
+//   {
+//     id: 2,
+//     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+//     role: "Open Source Contributor ",
+//     company: "GirlScript Summer of Code",
+//     date: "May 2023 - Present",
+//     desc: "Contributing to different open-source projects and learn from industry experts",
+//     doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+//   },
+//   {
+//     id: 3,
+//     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/gdsc.jpeg?alt=media&token=c162329c-efaa-4be8-a173-8d3f4c48ea70",
+//     role: "Android Developer",
+//     company: "DSC KIIT",
+//     date: "Nov2021 - Present",
+//     desc: "As an Android developer at the Google Developers Student Club (GDSC), I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology. Through my involvement with GDCS, I have also had the opportunity to host and participate in numerous events, including hackathons, study jams, and workshops.",
+//     skills: [
+//       "Leadership",
+//       "Mobile Application Development",
+//       "Kotlin",
+//       "XML",
+//       "Figma",
+//     ],
+//   },
+//   {
+//     id: 4,
+//     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Rudraksha.jpeg?alt=media&token=8f83f41e-d0a1-486f-9c7f-dd4cd1d09e8d",
+//     role: "Android Developer Intern",
+//     company: "Rudraksha Welffare Foundation",
+//     date: "June 2021 - Oct 2021",
+//     desc: "As an Android Developer Intern at Rudraksha Welfare Foundation from June 2021 to October 2021, I gained valuable hands-on experience in application development. During my tenure, I contributed to the development of the application's back-end cloud functions using Firebase and implemented a front-end with the MVVM structure, which was designed using Figma. I also added a one-to-one video call feature, conceptualized new features, and optimized app performance. This internship helped me enhance my skills in Android development and gave me an opportunity to work in a collaborative team environment.",
+//     skills: [
+//       "Android",
+//       "Java",
+//       "Kotlin",
+//       "XML",
+//       "Node Js",
+//       "Cloud Firestore",
+//       "Firebase",
+//       "Figma",
+//     ],
+//     doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/WhatsApp%20Image%202023-05-05%20at%2012.07.39%20AM.jpeg?alt=media&token=9f0e1648-568b-422d-bd0b-1f125f722245",
+//   },
+// ];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://i.postimg.cc/Hx8XcPf0/logobig.jpg",
+    school: "Vishnu Institute of Technology, Bhimavaram",
+    date: "Nov 2021 - Apr 2025",
+    grade: "8.81 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Vishnu Institute of Technology, Bhimavaram. I have completed 6 semesters and have a CGPA of 8.81. I have knowledge in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 2,
+    img: "https://i.postimg.cc/TPpkdxJC/images.png",
+    school: "Narayana Junior College",
+    date: "Apr 2019 - Oct 2021",
+    grade: "95.9%",
+    desc: "I completed my class Intermediate at Narayana Junior College, Vijayawada, where I studied Physics, Mathematics and Chemistry wherein I achieved good results with 3532 in TS EAMCET(regional level exam), 4434 in AP EAPCET and 90.8 percentile in JEE MAINS.",
+    degree: "Intermediate(12) in Science",
+  },
+  {
+    id: 1,
+    img: "https://i.postimg.cc/y8wZqTJk/jawahar-navodaya-vidyalaya-krishna.png",
+    school: "Jawahar Navodaya School, Veleru",
+    date: "July 2014 - Mar 2019",
+    grade: "93.6%",
+    desc: "I completed my class 10 high school education at Jawahar Navodaya Vidyalaya, Veleru, Krishna district where I studied General subjects like Maths, Science and Social and languages.",
+    degree: "CBSE 10th",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Food Ordering Website",
+    date: "Jun 2024 - Present",
+    description:
+      "Developed a comprehensive food ordering platform featuring restaurant profiles, dynamic menu displays, search and filter capabilities, and cart management. Enhanced performance with lazy loading and improved user experience using shimmer UI effects. Utilized React.js for a dynamic interface, Redux Toolkit for efficient state management, and Material UI for a modern design while demonstrating advanced web development skills to create a seamless user experience.",
+    image:
+      "https://private-user-images.githubusercontent.com/23612271/334376156-631d4af7-5e30-4891-9dbd-897fdf8d500b.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjE4MjM0OTIsIm5iZiI6MTcyMTgyMzE5MiwicGF0aCI6Ii8yMzYxMjI3MS8zMzQzNzYxNTYtNjMxZDRhZjctNWUzMC00ODkxLTlkYmQtODk3ZmRmOGQ1MDBiLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA3MjQlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNzI0VDEyMTMxMlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTEwMjNlOThjNmEwYmUyNzFhMTkyMzNjNzA4YWUzYWZmNGFmOTEyMjU1MzdlNWU0YWQzNmU4ZDM0MTEwZDJlNmImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.Hstn-j2vs6yq61EBzJKUnLrXHR_iwmKeWmcuvNaeWGM",
+    tags: ["React Js", "API", "Material UI", "tailwind"],
+    category: "web app",
+    github:
+      "https://github.com/faisalkhandev/foodAppOnline-Namaste-React-?tab=readme-ov-file",
+    webapp: "https://foodappreactredux.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "Responsive Spotify Clone",
+    date: "Jan 2024 - Feb 2024",
+    description:
+      "I conceptualized and developed a Spotify clone application using HTML, CSS, and JavaScript. This project aimed to replicate the core features and user experience of the original Spotify platform while addressing the limitation of its unresponsiveness.",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/1/19/Spotify_logo_without_text.svg",
+    tags: ["HTML", "CSS", "Javascript"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Quiz-Earn",
+    webapp: "https://affancoder.github.io/Music_Player/",
+  },
+  {
+    id: 2,
+    title: "Breast Cancer Detection",
+    date: "Dec 2023",
+    description:
+      "Breast Cancer Detection project provides a comprehensive approach to predicting breast cancer using machine learning. By leveraging various algorithms and evaluation metrics, the project aims to deliver a highly accurate and reliable model that can assist in early diagnosis, thereby improving patient outcomes and aiding in timely medical intervention.",
+    image:
+      "https://i.postimg.cc/ZYMm2Hj6/Breast-Cancer-Detection-Machine-Learning-Project.webp",
+    tags: ["Machine learning", "Classification", "Evaluation"],
+    category: "machine learning",
+    github: "https://github.com/rishavchanda/Quiz-Earn",
+    webapp: "https://github.com/rishavchanda/Quiz-Earn",
+  },
+];
+
+// export const TimeLineData = [
+//   { year: 2017, text: "Started my journey" },
+//   { year: 2018, text: "Worked as a freelance developer" },
+//   { year: 2019, text: "Founded JavaScript Mastery" },
+//   { year: 2020, text: "Shared my projects with the world" },
+//   { year: 2021, text: "Started my own platform" },
+// ];
