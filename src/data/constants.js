c277be8a8aee68ad1cb26c2d5177cae7c08c74ab
Mourya <125553814+MouryaPeddineni@users.noreply.github.com>
2024-07-30 21:15:48 +0530

    Update resume in constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/MouryaPeddineni",
   resume:
-    "https://drive.google.com/file/d/1sh5ZOO90EEmdUpeHLaqpI7ObxSq533YQ/view?usp=sharing",
+    "https://drive.google.com/file/d/1ezszwNGWXEQsDlxCVo_xGFMfDkpRVh17/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/mourya-peddineni-b19115233/",
   twitter: "https://twitter.com/MouryaP123",
 };
